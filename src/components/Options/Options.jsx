2e352487e386b30da9c5f07f84c27d10e4ffea38
JuliaLuk <julia1lukasiewicz@gmail.com>
2024-01-18 14:44:35 -0600

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,7 +4,6 @@ export const OptionsBtn = () => {
   const [good, setGoods] = useState(0);
   const [neutral, setNeutrals] = useState(0);
   const [bad, setBads] = useState(0);
-  const [total, setTotal] = useState(0);
 
   const handleGoods = () => {
     setGoods(good + 1);
@@ -18,16 +17,12 @@ export const OptionsBtn = () => {
     setBads(bad + 1);
   };
 
-  const handleTotal = () => {
-    setTotal(good - bad);
-  };
-
   return (
     <>
       <button onClick={handleGoods}>Good: {good}</button>
       <button onClick={handleNeutrals}>Neutral: {neutral}</button>
       <button onClick={handleBads}>Bad: {bad}</button>
-      <button onClick={handleTotal}>Reset: {total}</button>
+      <button onClick={() => setGoods(0)}>Reset</button>
     </>
   );
 };
